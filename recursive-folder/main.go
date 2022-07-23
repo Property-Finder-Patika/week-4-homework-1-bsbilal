2b3a02ad4643259598d1512d90cf9a040cb6233e
Basulas Bilal <bilal530@gmaill.com>
2022-07-23 10:20:46 +0300

    recursive ok

---
+++ recursive-folder/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	searchDir := "c:/CactusFlutter"
+	fileList := []string{}
+	err := filepath.Walk(searchDir, func(path string, f os.FileInfo, err error) error {
+		fileList = append(fileList, path)
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	for _, file := range fileList {
+		fmt.Println(file)
+	}
+}
